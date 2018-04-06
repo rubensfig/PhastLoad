e7e9a8e90de18cbaff04bd7cacadcacbf3f7301b
rubensfig <r.figueiredo.52@gmail.com>
2018-04-06 21:37:37 +0100

    start using mariadb

--- phastload/phastload/development.py
+++ phastload/phastload/development.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'phastload.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : 'hackathon',
+        'USER'     : 'hackathon',
+        'PASSWORD' : 'hackathon',
+        'HOST'     : 'mariadb',
+        'PORT'     : '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
