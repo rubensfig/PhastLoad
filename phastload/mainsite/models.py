3b0ba7e5e3f3e2b5fca911b850efbf40684dcfac
rubensfig <r.figueiredo.52@gmail.com>
2018-04-07 22:11:23 +0100

    started on relational model for DB

--- phastload/mainsite/models.py
+++ phastload/mainsite/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model): 
+class Beaches(models.Model): 
+
+    BEACH_TYPES = (
+        ('B', 'Beginner'),
+        ('M', 'Medium'),
+        ('A', 'Advanced'),
+    )
+
     id = models.CharField(max_length=30, primary_key=True)
     name = models.CharField(max_length=30)
+    latX = models.DecimalField(max_length=30)
+    latY = models.DecimalField(max_length=30)
+    cond = models.CharField(max_length=30, choices=BEACH_TYPES)
 
-class Files(models.Model):
-    file_id = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
+class Users(models.Model):
+
+    EXPERIENCE = (
+        ('B', 'Beginner'),
+        ('M', 'Medium'),
+        ('A', 'Advanced'),
+    )
+
+    id = models.CharField(max_length=30, primary_key=True)
+    exp = models.CharField(max_length=30, choices=EXPERIENCE)
