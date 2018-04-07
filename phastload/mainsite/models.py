e33fb8c8105d8578e49cde907317e0c157cbf1c5
Rubens Figueiredo <ee12010@fe.up.pt>
2018-04-07 17:08:48 +0100

    Database (#5)
    * start using mariadb
    
    * fixes on templates on docker container; fix dev env; add db stuff
    
    * migrations complete; use mariadb on the other container
    
    * use gui to manage DB

--- phastload/mainsite/models.py
+++ phastload/mainsite/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Files:
-    def __init__(self, uuid, expiration, ac):
-        self.uuid = uuid
-        self.expiration = expiration
-        self.access_code = ac
+class Users(models.Model): 
+    id = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=30)
 
-    def deletion(self):
-        if self.expiration == 0:
-            return True
-
-        return False
+class Files(models.Model):
+    file_id = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
--- phastload/phastload/development.py
+++ phastload/phastload/development.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
+    'phoneapp',
 ]
 
 MIDDLEWARE = [
@@ -71,9 +73,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'phastload.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 DATABASES = {
     'default': {
@@ -81,7 +89,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
